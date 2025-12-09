0096bff826b970d72787c593609145137fc6e2e0
FathimatusZN <fathimatus.zn18@gmail.com>
2025-12-09 14:47:09 +0700

    feat(seeder): add default weight values to criteria seeder
    - Include pre-calculated AHP weights in criteria data
    - Add bobot field with default values (C1: 0.26136, C2: 0.04795, etc)
    - Ensure criteria have valid weights after fresh installation
    - Maintain consistency with RelKriteriaSeeder results

--- database/seeders/KriteriaSeeder.php
+++ database/seeders/KriteriaSeeder.php
@@ -1,4 +1,5 @@
 <?php
+// database/seeders/KriteriaSeeder.php
 
 namespace Database\Seeders;
 
@@ -10,15 +11,43 @@ class KriteriaSeeder extends Seeder
     public function run()
     {
         $data = [
-            ['kode_kriteria' => 'C1', 'nama_kriteria' => 'Biaya',        'atribut' => 'Cost'],
-            ['kode_kriteria' => 'C2', 'nama_kriteria' => 'Aksesibilitas', 'atribut' => 'Benefit'],
-            ['kode_kriteria' => 'C3', 'nama_kriteria' => 'Fasilitas',    'atribut' => 'Benefit'],
-            ['kode_kriteria' => 'C4', 'nama_kriteria' => 'Jarak',       'atribut'  => 'Cost'],
-            ['kode_kriteria' => 'C5', 'nama_kriteria' => 'Rating',       'atribut' => 'Benefit'],
+            [
+                'kode_kriteria' => 'C1',
+                'nama_kriteria' => 'Biaya',
+                'atribut' => 'Cost',
+                'bobot' => 0.26136
+            ],
+            [
+                'kode_kriteria' => 'C2',
+                'nama_kriteria' => 'Aksesibilitas',
+                'atribut' => 'Benefit',
+                'bobot' => 0.04795
+            ],
+            [
+                'kode_kriteria' => 'C3',
+                'nama_kriteria' => 'Fasilitas',
+                'atribut' => 'Benefit',
+                'bobot' => 0.39998
+            ],
+            [
+                'kode_kriteria' => 'C4',
+                'nama_kriteria' => 'Jarak',
+                'atribut' => 'Cost',
+                'bobot' => 0.19982
+            ],
+            [
+                'kode_kriteria' => 'C5',
+                'nama_kriteria' => 'Rating',
+                'atribut' => 'Benefit',
+                'bobot' => 0.09090
+            ],
         ];
 
         foreach ($data as $item) {
-            Kriteria::updateOrCreate(['kode_kriteria' => $item['kode_kriteria']], $item);
+            Kriteria::updateOrCreate(
+                ['kode_kriteria' => $item['kode_kriteria']],
+                $item
+            );
         }
     }
 }
